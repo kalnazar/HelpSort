5e96412df2b3f4348c23e3042ae30a5cb8112885
Sayat Kalnazar <kalnazar.sayat@gmail.com>
2025-12-08 22:10:34 +0500

    Comments were removed and requirements were updated

--- server/utils/preprocessing_utils.py
+++ server/utils/preprocessing_utils.py
@@ -4,14 +4,8 @@ import re
 def preprocessing_fn(text: str) -> str:
     if not isinstance(text, str):
         text = str(text)
-
-    # to lower
     text = text.lower()
-
-    # remove simple HTML tags
     text = re.sub(r"<[^>]+>", " ", text)
-
-    # collapse multiple whitespace into single space
     text = re.sub(r"\s+", " ", text)
-
     return text.strip()
+
